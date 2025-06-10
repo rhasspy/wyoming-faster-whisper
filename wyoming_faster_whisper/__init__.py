c79a6a5fb73d124d4174d8f4dac7b95af50708e6
Michael Hansen <mike@rhasspy.org>
2025-06-10 09:36:50 -0500

    Fix version file

--- wyoming_faster_whisper/__init__.py
+++ wyoming_faster_whisper/__init__.py
@@ -1,9 +1,6 @@
 """Wyoming server for faster-whisper."""
-from pathlib import Path
+from importlib.metadata import version
 
-_DIR = Path(__file__).parent
-_VERSION_PATH = _DIR / "VERSION"
-
-__version__ = _VERSION_PATH.read_text(encoding="utf-8").strip()
+__version__ = version("wyoming_faster_whisper")
 
 __all__ = ["__version__"]
